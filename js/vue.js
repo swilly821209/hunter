9ccc2f7b1435b5444114df65843b15790384ae24
willy <swilly821209@icloud.com>
2019-02-12 20:19:25 +0800

    tooltip

--- js/vue.js
+++ js/vue.js
@@ -1,15 +1,18 @@
+Vue.component('person-tooltip', {
+  template:'#persontooltip'
+});
 const app = new Vue({
     el: '#app',
     data: {
-        month: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Spt", "Oct", "Nov", "Dec"]
+        month: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Spt', 'Oct', 'Nov', 'Dec']
     },
     computed: {
         newtime() {
-            var dates = new Date();
-            var m = dates.getMonth();
-            let mon = this.month[m]
-            var date = dates.getDate() + 1;
-            return `${mon} ${date}`
+            let dates = new Date();
+            let m = dates.getMonth();
+            let mon = this.month[m];
+            let date = dates.getDate() + 1;
+            return `${mon} ${date}`;
         },
     }
 })
